03c355e3d899250a38fc0dad3077f95c91f9e93c
Dmytro Tyshchenko <kwazar2008@ukr.net>
2019-10-19 09:16:52 +0300

    annotation+template

--- src/Controller/HomeWorkController.php
+++ src/Controller/HomeWorkController.php
@@ -3,13 +3,23 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class HomeWorkController
+class HomeWorkController extends AbstractController
 {
-    public function appYaml()
+    public function yaml()
     {
         return new Response(
             '<html><body>Yaml</body></html>'
         );
+    }
+
+    /**
+     * @Route("/annotation")
+     */
+    public function annotation()
+    {
+        return $this->render('templ.html.twig');
     }
 }
