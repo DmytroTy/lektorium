b5916af930e425beab37974a01cbd537166ad24d
Dmytro Tyshchenko <kwazar2008@ukr.net>
2019-11-18 16:25:21 +0200

    fix

--- src/Controller/StaffController.php
+++ src/Controller/StaffController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Staff;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
