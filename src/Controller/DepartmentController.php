2042e2949f117886e228a312708b93807979ab8b
Dmytro Tyshchenko <kwazar2008@ukr.net>
2019-11-18 16:52:55 +0200

    templates

--- src/Controller/DepartmentController.php
+++ src/Controller/DepartmentController.php
@@ -3,8 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\Department;
-use Doctrine\ORM\EntityManagerInterface;
+use App\Form\DepartmentType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -14,7 +15,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class DepartmentController extends AbstractController
 {
     /**
-     * @Route("/", name="department_list", methods={"GET"})
+     * @Route("/", name="department_index", methods={"GET"})
      */
     public function index(): Response
     {
@@ -22,60 +23,75 @@ class DepartmentController extends AbstractController
             ->getRepository(Department::class)
             ->findAll();
 
-        if (!$departments) {
-            throw $this->createNotFoundException(
-                'No department found'
-            );
-        }
-
-        return new Response('Check out this great departments');
+        return $this->render('department/index.html.twig', [
+            'departments' => $departments,
+        ]);
     }
 
     /**
-     * @Route("/{id}", name="department_show", requirements={"id"="\d+"}, methods={"GET"})
+     * @Route("/new", name="department_new", methods={"GET","POST"})
+     */
+    public function new(Request $request): Response
+    {
+        $department = new Department();
+        $form = $this->createForm(DepartmentType::class, $department);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($department);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('department_index');
+        }
+
+        return $this->render('department/new.html.twig', [
+            'department' => $department,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="department_show", methods={"GET"})
      */
     public function show(Department $department): Response
     {
-        return new Response('Check out this great department: '.$department->getTitle());
+        return $this->render('department/show.html.twig', [
+            'department' => $department,
+        ]);
     }
 
     /**
-     * @Route("/new", name="department_create", methods={"GET"})
+     * @Route("/{id}/edit", name="department_edit", methods={"GET","POST"})
      */
-    public function create(EntityManagerInterface $em): Response
+    public function edit(Request $request, Department $department): Response
     {
-        $department = new Department();
+        $form = $this->createForm(DepartmentType::class, $department);
+        $form->handleRequest($request);
 
-        $department->setTitle('Department name')
-            ->setDescription('Description')
-            ->setTeamLead('TeamLead');
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
 
-        $em->persist($department);
-        $em->flush();
+            return $this->redirectToRoute('department_index');
+        }
 
-        return new Response('Saved new department with id '.$department->getId());
+        return $this->render('department/edit.html.twig', [
+            'department' => $department,
+            'form' => $form->createView(),
+        ]);
     }
 
     /**
-     * @Route("/{id}/edit", name="department_update", methods={"GET"})
+     * @Route("/{id}", name="department_delete", methods={"DELETE"})
      */
-    public function update(Department $department, EntityManagerInterface $em): Response
+    public function delete(Request $request, Department $department): Response
     {
-        $department->setTitle($department->getTitle().' edited');
+        if ($this->isCsrfTokenValid('delete'.$department->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($department);
+            $entityManager->flush();
+        }
 
-        $em->flush();
-
-        return $this->redirectToRoute('department_show', ['id' => $department->getId()]);
-    }
-
-    /**
-     * @Route("/{id}/remove", name="department_delete")
-     */
-    public function delete(Department $department, EntityManagerInterface $em): Response
-    {
-        $em->remove($department);
-        $em->flush();
-
-        return new Response('Department was deleted successfully');
+        return $this->redirectToRoute('department_index');
     }
 }
